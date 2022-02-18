8ef6f24e25ce474fa778add768ceabdefd418ffc
DEVANSH <79394737+Devansh20055@users.noreply.github.com>
2022-02-19 00:22:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
-os.system("git clone https://github.com/LEGENDXOP/sessionhack_bot")
+os.system("git clone https://github.com/Devansh20055/hacker")
 os.system("cd sessionhack_bot && python bot.py")
